67f05114618dffedd01e4305585ac842d4b8f6eb
David Clarke <david.clarke@rea-group.com>
2012-09-09 11:03:46 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudSeedRailsStack::Application.config.secret_token = 'da1cd0855ceb229323c609c0480b13d112b02091fcb0b24210bd72274eff32792c5160a34dcc20ba996a229a51590c3746dfe1fe10fa808423336348f6339342'
